dee43d78a58b6a1560ecd90da7ec6df47533bbef
Alex <legoff.alexandre06@gmail.com>
2020-09-13 19:52:28 -0400

    template to code

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i=a; i<b; i++)
+#define REP(a, b) FOR(i, a, b)
+
+typedef long long ll;
+typedef vector<int> vi;
+
+template <typename ...Args>
+void write(Args && ...args) {
+	(cout << ... << args);
+}
+
+template <typename ...Args>
+void read(Args && ...args) {
+	(cin >> ... >> args);
+}
+
+void test_case() {
+	
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	return 0;
+}
